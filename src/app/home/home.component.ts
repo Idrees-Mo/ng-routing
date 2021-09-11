2c17f740246a7d0869c7a54bb1007d2f0e575d49
Mu Idrees <idris@y7mail.com>
2021-09-11 12:04:28 +0930

    dynamic queryParams & fragments

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -18,6 +18,8 @@ export class HomeComponent implements OnInit {
 
   onEditServer(id) {
     // some calculations
-    this.router.navigate(["servers", id, "edit"]);
+    this.router.navigate(["servers", id, "edit"], {
+      queryParams: { allowEdit: "1" },
+    });
   }
 }
